c2412d2c0d979155ef2ee4ac5494eb5a5616e11e
Robert Lippens <Robert.Lippens@infospace.com>
2014-04-08 12:09:27 -0700

    initial commit

---
+++ DevMachineSetupApp/CreateAGitHubAccount.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DevMachineSetupApp
+{
+    /// <summary>
+    /// Interaction logic for CreateAGitHubAccount.xaml
+    /// </summary>
+    public partial class CreateAGitHubAccount : Page
+    {
+        public CreateAGitHubAccount()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenGitHubInBrowser(object sender, RoutedEventArgs e)
+        {
+            var gitHubWindow = new WebBrowser();
+            gitHubWindow.Show();
+        }
+
+        public void DownloadNewPrograms(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Content = new DownloadNecessaryPrograms();
+        }
+    }
+}
---
+++ DevMachineSetupApp/CreateHomeDirectory.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace DevMachineSetupApp
+{
+    /// <summary>
+    /// Interaction logic for CreateHomeDirectory.xaml
+    /// </summary>
+    public partial class CreateHomeDirectory : Page
+    {
+        public CreateHomeDirectory()
+        {
+            InitializeComponent();
+        }
+
+        public void SetupAGitHubAccount(object sender, RoutedEventArgs e)
+        {
+            if (FileNameIsInvalid())
+                this.ErrorSection.Text = "Folder Name contains invalid characters or is empty.\n  Please verify the folder name is alphanumeric.";
+            else
+            {
+                Directory.CreateDirectory(string.Format("C:\\{0}", FolderNameTextbox.Text));
+                Window.GetWindow(this).Content = new CreateAGitHubAccount();
+            }
+        }
+
+        public bool FileNameIsInvalid() {
+            Match checkForInvalidChars = Regex.Match(FolderNameTextbox.Text, @"\W|_",RegexOptions.IgnoreCase);
+
+            return (string.IsNullOrEmpty(FolderNameTextbox.Text) || checkForInvalidChars.Success);
+        }
+    }
+}
